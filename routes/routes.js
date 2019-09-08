0d1e20835adb806bf81d89e02beda56e71baac8e
Bhavyam Goyal <bhavyam.kr.goyal@gmail.com>
2019-09-08 11:41:18 +0530

    routes file setup complete

--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,30 @@
-const routes = require('express').Router();
+var routes = require('express').Router();
 var testRouter = require('./test');
-var mailVerification=require('../FrameWork/UserVerification/FW_MailVerification');
+var mailVerification = require('../FrameWork/UserVerification/FW_MailVerification');
 mailVerification.SendVerificationEmail
 
 // to setup the name of the database and change server settings change details in server settings
 //uncomment 2 lines bellow if you want email verification
 
-routes.get('/checkVerification/:emailid',mailVerification.CheckVerification);
-routes.get('/verifyEmail/:key',mailVerification.UpdateVerification);
+routes.get('/checkVerification/:emailid', mailVerification.CheckVerification);
+routes.get('/verifyEmail', mailVerification.UpdateVerification);
 
-// to verify mail call the funtion SendVerificationmail in FW_MailVerification.js while registering user and pass email in it.
+// to verify mail call the funtion SendVerificationmail in FW_MailVerification.js while registering user and pass email in it like bellow.
 
-routes.post('/RegisterUser',function (req, res, next) {
-    console.log("[routes]Sending email to "+req.body.emailID);
-    mailVerification.SendVerificationEmail(req.body.emailID);
-    //register other parameters in you user table here
-    res.send("email sent");
+// routes.post('/RegisterUser', function (req, res, err) {
+   
+//         console.log("\n\n[routes]RegisterUser Post Request Data " + JSON.stringify(req.body));
+//         mailVerification.SendVerificationEmail(req.body.emailID);
+//         res.send({ message: "email sent" });
+//         //register other parameters in you user table here
+
+// });
+
+routes.get('/', function (req, res, next) {
+    res.send("1234");
 });
 
 
-
-routes.use('/test', testRouter );
+routes.use('/test', testRouter);
 
 module.exports = routes;
